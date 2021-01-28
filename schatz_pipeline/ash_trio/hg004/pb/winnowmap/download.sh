cfe428b239dae56d371cd54c8564d39d58c93071
Melanie Kirsche <mjkirsche@gmail.com>
2021-01-28 10:39:52 -0500

    Add CLR HG002 trio download scripts

---
+++ schatz_pipeline/ash_trio/hg004/pb/winnowmap/download.sh
@@ -0,0 +1,2 @@
+BINDIR=$(dirname "$(readlink -f "$0" || echo "$(echo "$0" | sed -e 's,\\,/,g')")")
+scp mkirsch5@login.rockfish.jhu.edu:/scratch4/mschatz1/Jasmine/analysis/ASHKENAZIvGRCh38_wm/svs/refined/HG004vGRCh38_wm_PB_sniffles.s2l20.refined.nSVtypes.ism.vcf $BINDIR
