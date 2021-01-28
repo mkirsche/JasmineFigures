7648aa4f60ba23f53d42f1e9f7ad742221473cd3
Melanie Kirsche <mjkirsche@gmail.com>
2021-01-28 10:26:28 -0500

    Add downloads for winnowmap baseline max_dist in hg002 trio

---
+++ schatz_pipeline/ash_trio/hg004/ont/winnowmap/download.sh
@@ -0,0 +1,2 @@
+BINDIR=$(dirname "$(readlink -f "$0" || echo "$(echo "$0" | sed -e 's,\\,/,g')")")
+scp mkirsche@jhu.edu@gateway2.marcc.jhu.edu:/scratch/groups/mschatz1/aganezov/CHM13/variants/noAD_GRCh38_ref/GIABvGRCh38_wm/svs/refined/HG004vGRCh38_wm_ONT_sniffles.s2l20.refined.nSVtypes.ism.vcf $BINDIR
